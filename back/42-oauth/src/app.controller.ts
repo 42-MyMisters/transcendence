e79a007a081b6354cd49b940e484276fca0ae291
Yoma <codeyoma@gmail.com>
2023-03-04 04:07:51 +0900

    [oauth] default passport, jwt token

--- back/42-oauth/src/app.controller.ts
+++ back/42-oauth/src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+// import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private authService: AuthService,
+    private readonly appService: AppService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req: any) { // NOTE:: any?
+    // return req.user;
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: any) {
+    return req.user;
   }
 }
---
+++ back/42-oauth/src/auth/jwt-auth.guard.ts
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import {
+	ExecutionContext,
+	Injectable,
+	UnauthorizedException,
+  } from '@nestjs/common';
+  import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+	canActivate(context: ExecutionContext) {
+		// Add your custom authentication logic here
+		// for example, call super.logIn(request) to establish a session.
+		return super.canActivate(context);
+	  }
+
+	  handleRequest(err: any, user: any, info: any) {
+		// You can throw an exception based on either "info" or "err" arguments
+		if (err || !user) {
+		  throw err || new UnauthorizedException();
+		}
+		return user;
+	  }
+}
