b15adbf04f96e3c08a08bd953ca1df10e154fcf2
Woo Joo Chae <enaenen@naver.com>
2023-03-09 16:23:20 +0900

    config module added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as config from 'config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const serverConfig = config.get('server');
+  const port = serverConfig.port;
+  await app.listen(port);
 }
 bootstrap();
