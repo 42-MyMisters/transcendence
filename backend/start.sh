ac25814d893decce76464673b9bbb408f47924d6
Woo Joo Chae <enaenen@naver.com>
2023-03-12 03:55:33 +0900

    jwt & passport install script

--- backend/start.sh
+++ backend/start.sh
@@ -10,6 +10,9 @@ npm install config --save --prefix /data/back
 ## TYPEORM
 npm install pg typeorm @nestjs/typeorm --save
 
+## JWT, Passport
+npm install @nestjs/jwt @nestjs/passport passport passport-jwt --save
+
 npm run start:dev --prefix /data/back
 
 
