20d9b1124f163174a050f098d498d42bfa5c4905
Woo Joo Chae <enaenen@naver.com>
2023-03-12 02:32:05 +0900

    npm typeorm module added

--- backend/start.sh
+++ backend/start.sh
@@ -4,7 +4,11 @@ nvm install --lts
 
 npm i -g @nestjs/cli
 
+## CONFIG
 npm install config --save --prefix /data/back
+
+## TYPEORM
+npm install pg typeorm @nestjs/typeorm --save
 
 npm run start:dev --prefix /data/back
 
