24f57272a5ead397864ffc854a6659f3421ccad3
Woo Joo Chae <enaenen@naver.com>
2023-03-15 19:08:47 +0900

    organize

--- backend/start.sh
+++ backend/start.sh
@@ -8,13 +8,13 @@ npm i -g @nestjs/cli
 npm install config --save --prefix /data/back
 
 ## TYPEORM
-npm install pg typeorm @nestjs/typeorm --save
+npm install pg typeorm @nestjs/typeorm --save --prefix /data/back
 
 ## JWT, Passport
-npm install @nestjs/jwt @nestjs/passport passport passport-jwt --save
+npm install @nestjs/jwt @nestjs/passport passport passport-jwt --save --prefix /data/back
 
 ## Class Validator
-npm i --save class-validator class-transformer
+npm i --save class-validator class-transformer --prefix /data/back
 
 npm run start:dev --prefix /data/back
 
