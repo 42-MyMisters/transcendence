78e3c640f71ece5336beb6cd4e3db8006d449a6f
Seungduk Seo <seseo@student.42seoul.kr>
2023-03-17 23:26:55 +0900

    package updated. auth redirection added

--- backend/back/src/auth/auth.controller.ts
+++ backend/back/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Redirect } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -6,8 +6,12 @@ export class AuthController {
 	
 	constructor(private authService: AuthService) {}
 
+	@Get('/redir')
+	@Redirect('https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d66732cf3b341b21ff4c3596d13133df0c2365a3c855440ea26cb9b60f52c668&redirect_uri=http://localhost:4000/auth/login/&response_type=code', 301)
+	intra() {
+	}
+
 	@Get('/login')
-	// @Redirect()
 	intraSignIn(@Query('code') code: string) : Promise<{accessToken: string}>{
 		return this.authService.intraSignIn(code);
 	}
