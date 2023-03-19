92514a1357a07b557a86ac3abad63e2b581ede98
YunKi <yunki.dev@gmail.com>
2023-03-19 11:24:20 +0900

    [add] TopBar BackGround

---
+++ frontend/front/src/components/TopBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../css/TopBar.css";
+
+export default function TopBar() {
+  return (
+    <div className="TopBarBG">
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
