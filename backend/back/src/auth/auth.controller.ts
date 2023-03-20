424754f6a43d20b4d498b84e1572f63859b9ee91
Seungduk Seo <seseo@student.42seoul.kr>
2023-03-20 23:58:41 +0900

    redir path added from config
    Makefile up will copy development.yml

--- backend/back/src/auth/auth.controller.ts
+++ backend/back/src/auth/auth.controller.ts
@@ -1,19 +1,21 @@
 import { Controller, Get, Query, Redirect } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import config from 'config';
 
 @Controller('auth')
 export class AuthController {
-	
-	constructor(private authService: AuthService) {}
+	constructor(private authService: AuthService) {
+	}
 
 	@Get('/redir')
-	@Redirect('https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d66732cf3b341b21ff4c3596d13133df0c2365a3c855440ea26cb9b60f52c668&redirect_uri=http://localhost:4000/auth/login/&response_type=code', 301)
-	intra() {
+	@Redirect('https://api.intra.42.fr/oauth/authorize?client_id=' + config.get<any>('intra').client_id + '&redirect_uri=' + config.get<any>('intra').redirect_uri + '&response_type=code', 301)
+	intra(){
 	}
 
 	@Get('/login')
 	intraSignIn(@Query('code') code: string) : Promise<{accessToken: string}>{
 		return this.authService.intraSignIn(code);
 	}
+
 }
 
