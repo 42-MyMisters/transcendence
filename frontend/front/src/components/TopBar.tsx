926104e120c737d76a11eccf4c67917d1e962a43
YunKi <yunki.dev@gmail.com>
2023-03-23 16:57:44 +0900

    [fix] TopBar visual

--- frontend/front/src/components/TopBar.tsx
+++ frontend/front/src/components/TopBar.tsx
@@ -4,8 +4,33 @@ import "../css/TopBar.css";
 export default function TopBar() {
   return (
     <div className="TopBarBG">
-      <div></div>
-      <div></div>
+      <LogoBtn />
+      <div className="TopBarBtnAlign">
+        <ChatBtn />
+        <QueueBtn />
+        <ProfileBtn />
+        <LogoutBtn />
+      </div>
     </div>
   );
+}
+
+function LogoBtn() {
+  return <div className="TopBarBtn TopBarLogo">MyMisters</div>;
+}
+
+function ChatBtn() {
+  return <div className="TopBarBtn">Chat</div>;
+}
+
+function QueueBtn() {
+  return <div className="TopBarBtn">Queue</div>;
+}
+
+function ProfileBtn() {
+  return <div className="TopBarBtn">Profile</div>;
+}
+
+function LogoutBtn() {
+  return <div className="TopBarBtn">Logout</div>;
 }
