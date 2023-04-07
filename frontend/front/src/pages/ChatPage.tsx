50f7102ac7586cd5247ab57e0e69b62eaa4521b0
42yotak <yotak@student.42seoul.kr>
2023-04-07 18:35:59 +0900

    MYM-63 set allModal

--- frontend/front/src/pages/ChatPage.tsx
+++ frontend/front/src/pages/ChatPage.tsx
@@ -15,15 +15,29 @@ import ChatRoomList from "../components/ChatPage/ChatRoomList";
 import ChatUserList from "../components/ChatPage/ChatUserList";
 import ChatArea from "../components/ChatPage/ChatArea";
 import ChatRoomUserList from "../components/ChatPage/ChatRoomUserList";
+import { useState } from "react";
+import UserInfoModal from "../components/ChatPage/UserInfoModal";
+import RoomModal from "../components/ChatPage/RoomModal";
+import RoomInviteModal from "../components/ChatPage/RoomInviteModal";
 
 export default function ChatPage() {
+  const [showUserInfoModal, setShowUserInfoModal] = useState(false);
+  const [showRoomModal, setShowRoomModal] = useState(false);
+  const [showInviteModal, setShowInviteModal] = useState(false);
+
   return (
     <BackGround>
       <TopBar />
-      <ChatRoomList />
+      {showUserInfoModal ? <UserInfoModal setUserInfoModal={setShowUserInfoModal} /> : null}
+      {showRoomModal ? <RoomModal setRoomModal={setShowRoomModal} /> : null}
+      {showInviteModal ? <RoomInviteModal setInviteModal={setShowInviteModal} /> : null}
+      <ChatRoomList setRoomModal={setShowRoomModal} />
       <ChatUserList />
       <ChatArea />
-      <ChatRoomUserList />
+      <ChatRoomUserList
+        setUserInfoModal={setShowUserInfoModal}
+        setInviteModal={setShowInviteModal}
+      />
     </BackGround>
   );
 }
