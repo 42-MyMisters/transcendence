c1b4454332f6185f6125d1bb61031b8a45b34446
Yoma <codeyoma@gmail.com>
2023-04-15 00:16:33 +0900

    MYM-76 add link

--- frontend/front/src/components/TopBar.tsx
+++ frontend/front/src/components/TopBar.tsx
@@ -1,4 +1,5 @@
 import "../styles/TopBar.css";
+import { Link, NavLink } from "react-router-dom";
 
 export default function TopBar() {
   return (
@@ -14,22 +15,44 @@ export default function TopBar() {
   );
 }
 
+function getNavLinkStyle({ isActive }: { isActive: boolean }) {
+  return {
+    textDecoration: isActive ? "underline" : undefined,
+  };
+}
+
 function LogoBtn() {
-  return <div className="TopBarBtn TopBarLogo">MyMisters</div>;
+  return (
+    <div className="TopBarBtn TopBarLogo">
+      <a href="https://github.com/42-MyMisters" target="_blank" rel="noreferrer">MyMisters</a>
+    </div>
+  );
 }
 
 function ChatBtn() {
-  return <div className="TopBarBtn">Chat</div>;
+  return (
+    <div className="TopBarBtn">
+      <NavLink to="/chat" style={getNavLinkStyle}>Chat</NavLink>
+    </div>
+  );
 }
 
 function QueueBtn() {
-  return <div className="TopBarBtn">Queue</div>;
+  return (
+    <div className="TopBarBtn">Queue</div>
+  );
 }
 
 function ProfileBtn() {
-  return <div className="TopBarBtn">Profile</div>;
+  return (
+    <div className="TopBarBtn">
+      <NavLink to="/profile" style={getNavLinkStyle}>Profile</NavLink>
+    </div>
+  );
 }
 
 function LogoutBtn() {
-  return <div className="TopBarBtn">Logout</div>;
+  return (
+    <div className="TopBarBtn">Logout</div>
+  );
 }
