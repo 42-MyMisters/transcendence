6a0717e942a263d673c5015414a9ec49afaf9363
42yotak <yotak@student.42seoul.kr>
2023-04-15 17:49:00 +0900

    MYM-81 change name modal on

---
+++ frontend/front/src/components/ProfilePage/ChangNameModal.tsx
@@ -0,0 +1,23 @@
+import { useAtom } from "jotai";
+
+import { changeNameModalAtom } from "../atom/ModalAtom";
+import "../../styles/ProfileModal.css";
+
+export default function ChangeNameModal() {
+  const [changeNameModal, setchangeNameModal] = useAtom(changeNameModalAtom);
+  return (
+    <>
+      <div className="ChangeNameModalBG"></div>
+      <div className="ChangeNameModal">
+        <div className="SaveNameForm">
+          <label htmlFor="SaveName">New Nickname</label>
+          <input id="SaveName" type="text"></input>
+        </div>
+        <button className="SaveName">Save</button>
+        <button className="SaveNameCancel" onClick={() => setchangeNameModal(false)}>
+          Cancel
+        </button>
+      </div>
+    </>
+  );
+}
