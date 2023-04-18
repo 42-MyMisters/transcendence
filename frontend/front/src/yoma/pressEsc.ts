f46f41a41261c0cac43cb423fd0639e9e76db224
Yoma <codeyoma@gmail.com>
2023-04-18 19:33:19 +0900

    ...

--- frontend/front/src/yoma/pressEsc.ts
+++ frontend/front/src/yoma/pressEsc.ts
@@ -1,11 +1,7 @@
 
 import { useEffect } from 'react';
 
-<<<<<<< HEAD
 export const PressEsc = (keys: string[], callback: () => void) => {
-=======
-export const PressEsc = (callback: () => void, keys: string[]) => {
->>>>>>> c3e81c85d0c39db2c07a2c4a8c3e61faee5aee87
 	const onKeyDown = (event: KeyboardEvent) => {
 		const wasAnyKeyPressed = keys.some((key: string) => event.key === key);
 		if (wasAnyKeyPressed) {
@@ -21,9 +17,3 @@ export const PressEsc = (callback: () => void, keys: string[]) => {
 	}, []);
 };
 
-<<<<<<< HEAD
-=======
-//   pressEsc(() => {
-//     setRoomModal(false);
-//   }, ["Escape"]);
->>>>>>> c3e81c85d0c39db2c07a2c4a8c3e61faee5aee87
