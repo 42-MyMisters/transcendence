c105fe84241700369577d2d4c45bf70ed245bd06
Seungduk Seo <seseo@student.42seoul.kr>
2023-04-25 19:24:47 +0900

    MYM-74 merged from MYM-85

--- backend/back/src/login/login.module.ts
+++ backend/back/src/login/login.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from 'src/auth/auth.module';
-import { AuthService } from 'src/auth/auth.service';
-import { DatabaseModule } from 'src/database/database.module';
 import { UserModule } from 'src/user/user.module';
-import { UserService } from 'src/user/user.service';
 import { LoginController } from './login.controller';
 
 @Module({
--- backend/back/src/socket/events.module.ts
+++ backend/back/src/socket/events.module.ts
@@ -1,8 +1,4 @@
 import { Module } from '@nestjs/common';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import config from 'config';
-import { AuthModule } from 'src/auth/auth.module';
-import { AuthService } from 'src/auth/auth.service';
 import { UserModule } from 'src/user/user.module';
 import { EventsGateway } from './events.gateway';
 
