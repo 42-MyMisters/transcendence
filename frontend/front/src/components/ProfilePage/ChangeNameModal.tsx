af6853eda6fb0b66bc46da3397ed12e71d15a26c
42yotak <yotak@student.42seoul.kr>
2023-04-28 15:56:56 +0900

    MYM-104 [feat] nickname changed

--- frontend/front/src/components/ProfilePage/ChangeNameModal.tsx
+++ frontend/front/src/components/ProfilePage/ChangeNameModal.tsx
@@ -1,15 +1,40 @@
 import { useAtom } from "jotai";
 import { changeNameModalAtom } from "../atom/ModalAtom";
-import { PressKey } from "../../event/pressKey";
+import { UserAtom } from "../atom/UserAtom";
 
+import { useState } from "react";
+
+import { PressKey } from "../../event/pressKey";
 import "../../styles/ProfileModal.css";
+import { error } from "console";
 
 export default function ChangeNameModal() {
   const [changeNameModal, setchangeNameModal] = useAtom(changeNameModalAtom);
+  const [userInfo, setUserInfo] = useAtom(UserAtom);
+  const [newName, setNewName] = useState("");
 
   PressKey(["Escape"], () => {
     setchangeNameModal(false);
   });
+
+  const handleChangeName = () => {
+    const format = JSON.stringify({ nickname: newName });
+    console.log(format);
+    fetch("http://localhost:4000/user/nickname", {
+      credentials: "include",
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: format,
+    })
+      .then((response) => {
+        console.log(response);
+        userInfo.nickname = newName;
+        setchangeNameModal(false);
+      })
+      .catch((error) => {
+        console.log(`error: ${error}`);
+      });
+  };
 
   return (
     <>
@@ -17,9 +42,19 @@ export default function ChangeNameModal() {
       <div className="ChangeNameModal">
         <div className="SaveNameForm">
           <label htmlFor="SaveName">New Nickname</label>
-          <input id="SaveName" type="text"></input>
+          <input
+            id="SaveName"
+            type="text"
+            maxLength={8}
+            value={newName}
+            onChange={(e) => {
+              setNewName(e.target.value);
+            }}
+          ></input>
         </div>
-        <button className="SaveName">Save</button>
+        <button type="submit" className="SaveName" onClick={() => handleChangeName()}>
+          Save
+        </button>
         <button className="SaveNameCancel" onClick={() => setchangeNameModal(false)}>
           Cancel
         </button>
