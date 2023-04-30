52f894cf2bc274159f6ac774e8cc81c8b18fa688
YunKi <yunki.dev@gmail.com>
2023-04-30 19:23:45 +0900

    [add] GameResultModal

--- frontend/front/src/components/GamePage/GameResultModal.tsx
+++ frontend/front/src/components/GamePage/GameResultModal.tsx
@@ -1,10 +1,27 @@
+import { useAtom } from "jotai";
 import "../../styles/GameResultModal.css";
+import { gameResultModalAtom } from "../atom/ModalAtom";
 
-export default function GameResultModal() {
+export default function GameResultModal({
+  result,
+  leftScore,
+  rightScore,
+}: {
+  result: boolean;
+  leftScore: number;
+  rightScore: number;
+}) {
+  const [, setGameResultModal] = useAtom(gameResultModalAtom);
+
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className="GameResultModalBG">
+      <div className="GameResultModal">
+        <div className="GameResultModalTxt">{result ? "You Win!" : "You Lose"}</div>
+        <div className="GameResultModalScore">{`${leftScore} : ${rightScore}`}</div>
+        <button className="GameResultModalBtn" onClick={() => setGameResultModal(false)}>
+          OK
+        </button>
+      </div>
     </div>
   );
 }
