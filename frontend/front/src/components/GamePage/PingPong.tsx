cb31cf2bcf0771f0d4f3e99a7928a1249e593a69
yoma in 42 cluster <yongmkim@student.42seoul.kr>
2023-05-02 22:25:04 +0900

    MYM-101 add game event listener in PingPong

--- frontend/front/src/components/GamePage/PingPong.tsx
+++ frontend/front/src/components/GamePage/PingPong.tsx
@@ -8,25 +8,127 @@ import { Game } from "./Pong";
 import { GameCoordinateAtom, GameCoordinate } from "../atom/GameAtom";
 import { PressKey } from "../../event/pressKey";
 
+import * as game from '../../socket/game.socket';
+
 export default function PingPong() {
   const [coordinate, setCoordinate] = useAtom(GameCoordinateAtom);
 
-  PressKey(["ArrowUp"], () => {
-    let tmp = coordinate;
-    tmp.leftY -= 10;
-    setCoordinate(tmp);
-    Game(coordinate);
+  // PressKey(["ArrowUp"], () => {
+  //   let tmp = coordinate;
+  //   tmp.leftY -= 10;
+  //   setCoordinate(tmp);
+  //   Game(coordinate);
+  // });
+
+  // PressKey(["ArrowDown"], () => {
+  //   let tmp = coordinate;
+  //   tmp.leftY += 10;
+  //   setCoordinate(tmp);
+  //   Game(coordinate);
+  // });
+
+  // catch all incoming events
+  game.gameSocket.onAny((eventName, ...args) => {
+    console.log("incoming ", eventName, args);
   });
-  PressKey(["ArrowDown"], () => {
-    let tmp = coordinate;
-    tmp.leftY += 10;
-    setCoordinate(tmp);
-    Game(coordinate);
+
+  // catch all outgoing events
+  game.gameSocket.prependAny((eventName, ...args) => {
+    console.log("outgoing ", eventName, args);
   });
+
+  game.gameSocket.on("connect", () => {
+    if (game.gameSocket.connected) {
+      //This attribute describes whether the socket is currently connected to the server.
+      if (game.gameSocket.recovered) {
+        // any missed packets will be received
+      } else {
+        // new or unrecoverable session
+        console.log("gameSocket connected : " + game.gameSocket.id);
+      }
+    }
+  });
+
+  //https://socket.io/docs/v4/client-socket-instance/#disconnect
+  game.gameSocket.on("disconnect", (reason) => {
+    /**
+     *  BAD, will throw an error
+     *  gameSocket.emit("disconnect");
+    */
+    if (reason === "io server disconnect") {
+      // the disconnection was initiated by the server, you need to reconnect manually
+    }
+    // else the socket will automatically try to reconnect
+    console.log("gameSocket disconnected");
+  });
+
+  // the connection is denied by the server in a middleware function
+  game.gameSocket.on("connect_error", (err) => {
+    if (err.message === "unauthorized") {
+      // handle each case
+    }
+    console.log(err.message); // prints the message associated with the error
+  });
+
+  game.gameSocket.on('join-game', ({
+    uid_left,
+    p1,
+    uid_right
+  }: {
+    uid_left: string;
+    p1: number;
+    uid_right: string;
+  }) => {
+
+  });
+
+  useEffect(() => {
+    game.gameSocket.on('graphic', ({
+      p1,
+      ball_x,
+      ball_y,
+      p2
+    }: {
+      p1: number;
+      ball_x: number;
+      ball_y: number;
+      p2: number;
+    }) => {
+      let temp = coordinate;
+      temp = {
+        leftY: p1,
+        ballX: ball_x,
+        ballY: ball_y,
+        rightY: p2
+      }
+      setCoordinate(temp);
+      Game(coordinate);
+    });
+    return () => {
+      game.gameSocket.off('graphic');
+    };
+  }, [coordinate]);
 
   useEffect(() => {
     Game(coordinate);
   }, []);
+
+  // document.addEventListener('keydown', onKeyDown);
+  window.addEventListener('keydown', e => {
+    if (e.key === 'ArrowUp') {
+      game.emitUpPress();
+    } else if (e.key === 'ArrowDown') {
+      game.emitDownPress();
+    }
+  });
+
+  window.addEventListener('keyup', e => {
+    if (e.key === 'ArrowUp') {
+      game.emitUpRelease();
+    } else if (e.key === 'ArrowDown') {
+      game.emitDownRelease();
+    }
+  });
 
   return (
     <div className="QueueBackGround">
--- frontend/front/src/pages/GamePage.tsx
+++ frontend/front/src/pages/GamePage.tsx
@@ -8,17 +8,110 @@ import TopBar from "../components/TopBar";
 
 import { useAtom } from "jotai";
 import { isQueueAtom } from "../components/atom/GameAtom";
-import * as gameSocket from '../socket/game.socket';
+
+import { PressKey } from "../event/pressKey";
+import * as game from '../socket/game.socket';
+import { GameCoordinateAtom, GameCoordinate } from "../components/atom/GameAtom";
+
+import { Game } from "../components/GamePage/Pong";
 
 export default function GamePage() {
   const [showComponent, setShowComponent] = useState(true);
   const [isQueue, setIsQueue] = useAtom(isQueueAtom);
 
+  const [coordinate, setCoordinate] = useAtom(GameCoordinateAtom);
+
   if (isQueue === false) {
     console.log("gameSocket connect");
-    gameSocket.gameSocket.connect();
+    game.gameSocket.connect();
     setIsQueue(true);
   }
+
+  // // catch all incoming events
+  // game.gameSocket.onAny((eventName, ...args) => {
+  //   console.log("incoming ", eventName, args);
+  // });
+
+  // // catch all outgoing events
+  // game.gameSocket.prependAny((eventName, ...args) => {
+  //   console.log("outgoing ", eventName, args);
+  // });
+
+  // game.gameSocket.on("connect", () => {
+  //   if (game.gameSocket.connected) {
+  //     //This attribute describes whether the socket is currently connected to the server.
+  //     if (game.gameSocket.recovered) {
+  //       // any missed packets will be received
+  //     } else {
+  //       // new or unrecoverable session
+  //       console.log("gameSocket connected : " + game.gameSocket.id);
+  //     }
+  //   }
+  // });
+
+  // //https://socket.io/docs/v4/client-socket-instance/#disconnect
+  // game.gameSocket.on("disconnect", (reason) => {
+  //   /**
+  //    *  BAD, will throw an error
+  //    *  gameSocket.emit("disconnect");
+  //   */
+  //   if (reason === "io server disconnect") {
+  //     // the disconnection was initiated by the server, you need to reconnect manually
+  //   }
+  //   // else the socket will automatically try to reconnect
+  //   console.log("gameSocket disconnected");
+  // });
+
+  // // the connection is denied by the server in a middleware function
+  // game.gameSocket.on("connect_error", (err) => {
+  //   if (err.message === "unauthorized") {
+  //     // handle each case
+  //   }
+  //   console.log(err.message); // prints the message associated with the error
+  // });
+
+  // game.gameSocket.on('join-game', ({
+  //   uid_left,
+  //   p1,
+  //   uid_right
+  // }: {
+  //   uid_left: string;
+  //   p1: number;
+  //   uid_right: string;
+  // }) => {
+
+  // });
+
+  // game.gameSocket.on('graphic', ({
+  //   p1,
+  //   ball_x,
+  //   ball_y,
+  //   p2
+  // }: {
+  //   p1: number;
+  //   ball_x: number;
+  //   ball_y: number;
+  //   p2: number;
+  // }) => {
+
+  //   Game(coordinate);
+
+  // });
+
+  // PressKey(["ArrowUp"], () => {
+  //   let tmp = coordinate;
+  //   tmp.leftY -= 10;
+  //   setCoordinate(tmp);
+  //   Game(coordinate);
+  // });
+
+  // PressKey(["ArrowDown"], () => {
+  //   let tmp = coordinate;
+  //   tmp.leftY += 10;
+  //   setCoordinate(tmp);
+  //   Game(coordinate);
+  // });
+
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -29,10 +122,12 @@ export default function GamePage() {
     //   };
     // }, []);
   }, []);
+
   return (
     <BackGround>
       <TopBar />
-      <div>{showComponent ? <Waiting /> : <PingPong />}</div>
+      {/* <div>{showComponent ? <Waiting /> : <PingPong />}</div> */}
+      <PingPong />
     </BackGround>
   );
 }
