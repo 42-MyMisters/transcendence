b6d3642224f141593ad191b4f3852ce5bfb5a4b5
Woo Joo Chae <enaenen@naver.com>
2023-05-04 16:28:15 +0900

    MYM-115 refresh token response cookie add & DB ref token unmatch BadRequest

--- backend/back/src/auth/auth.service.ts
+++ backend/back/src/auth/auth.service.ts
@@ -1,160 +1,160 @@
-import { BadGatewayException, BadRequestException, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
-import config from 'config';
-import { authenticator } from 'otplib';
-import { User } from 'src/database/entity/user.entity';
-import { IntraTokenDto } from 'src/user/dto/IntraToken.dto';
-import { IntraUserDto } from 'src/user/dto/IntraUser.dto';
-import { TokenSetDto } from 'src/user/dto/TokenSet.dto';
+import {
+  BadGatewayException,
+  BadRequestException,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import * as bcrypt from "bcrypt";
+import config from "config";
+import { authenticator } from "otplib";
+import { User } from "src/database/entity/user.entity";
+import { IntraTokenDto } from "src/user/dto/IntraToken.dto";
+import { IntraUserDto } from "src/user/dto/IntraUser.dto";
+import { TokenSetDto } from "src/user/dto/TokenSet.dto";
 
 @Injectable()
 export class AuthService {
-	constructor(
-		private jwtService: JwtService,
-	) { }
+  constructor(private jwtService: JwtService) {}
 
-	async getUserInfoFromIntra(tokenObject: IntraTokenDto): Promise<IntraUserDto> {
-		const meUrl = 'https://api.intra.42.fr/v2/me';
-		try {
-			const response = await fetch(meUrl, {
-				method: 'GET',
-				headers: {
-					'Authorization': `Bearer ${tokenObject.access_token}`,
-				},
-			});
-			if (response.status < 200 || response.status >= 300) {
-				Logger.error('Intra Error');
-				throw new BadRequestException(`HTTP error! status: ${response.status}`);
-			}
-			const intraUserInfo: IntraUserDto = await response.json();
-			return intraUserInfo;
-		}
-		catch (error) {
-			Logger.error('Intra Error');
-			throw new BadGatewayException('Failed to fetch user information from Intra');
-		}
-	}
+  async getUserInfoFromIntra(
+    tokenObject: IntraTokenDto,
+  ): Promise<IntraUserDto> {
+    const meUrl = "https://api.intra.42.fr/v2/me";
+    try {
+      const response = await fetch(meUrl, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${tokenObject.access_token}`,
+        },
+      });
+      if (response.status < 200 || response.status >= 300) {
+        Logger.error("Intra Error");
+        throw new BadRequestException(`HTTP error! status: ${response.status}`);
+      }
+      const intraUserInfo: IntraUserDto = await response.json();
+      return intraUserInfo;
+    } catch (error) {
+      Logger.error("Intra Error");
+      throw new BadGatewayException(
+        "Failed to fetch user information from Intra",
+      );
+    }
+  }
 
-	async getTokenFromIntra(code: string): Promise<IntraTokenDto> {
-		const clientId = config.get<string>('intra.client_id');
-		const clientSecret = config.get<string>('intra.client_secret');
-		const redirect_uri = config.get<string>('intra.redirect_uri');
-		const url = config.get<string>('intra.url');
+  async getTokenFromIntra(code: string): Promise<IntraTokenDto> {
+    const clientId = config.get<string>("intra.client_id");
+    const clientSecret = config.get<string>("intra.client_secret");
+    const redirect_uri = config.get<string>("intra.redirect_uri");
+    const url = config.get<string>("intra.url");
 
-		const params = new URLSearchParams();
-		params.set('grant_type', 'authorization_code');
-		params.set('client_id', clientId);
-		params.set('client_secret', clientSecret);
-		params.set('code', code);
-		params.set('redirect_uri', redirect_uri);
+    const params = new URLSearchParams();
+    params.set("grant_type", "authorization_code");
+    params.set("client_id", clientId);
+    params.set("client_secret", clientSecret);
+    params.set("code", code);
+    params.set("redirect_uri", redirect_uri);
 
-		const response = await fetch(url, {
-			method: 'POST',
-			body: params
-		});
+    const response = await fetch(url, {
+      method: "POST",
+      body: params,
+    });
 
-		const intraToken : IntraTokenDto = await response.json();
-		if (response.status < 200 || response.status >= 300) {
-			Logger.error('Intra code error');
-			throw new BadRequestException(`HTTP error! status: ${response.status}`);
-		}
-		return intraToken;
-	}
+    const intraToken: IntraTokenDto = await response.json();
+    if (response.status < 200 || response.status >= 300) {
+      Logger.error("Intra code error");
+      throw new BadRequestException(`HTTP error! status: ${response.status}`);
+    }
+    return intraToken;
+  }
 
-	async intraSignIn(code: string) : Promise<IntraUserDto> {
-		const userToken = await this.getTokenFromIntra(code);
-		const userData = await this.getUserInfoFromIntra(userToken);
-		return userData;
-		// const curUser = await this.userService.getUserById(userData.id);
+  async intraSignIn(code: string): Promise<IntraUserDto> {
+    const userToken = await this.getTokenFromIntra(code);
+    const userData = await this.getUserInfoFromIntra(userToken);
+    return userData;
+    // const curUser = await this.userService.getUserById(userData.id);
 
-		// if (!this.userService.isUserExist(curUser)) {
-		// 	const newUser = await this.userService.addNewUser(userData);
-		// 	return newUser;
-		// }
-		// TODO: refreshToken 이 이미 있는경우 처리
-		// return curUser;
-	}
+    // if (!this.userService.isUserExist(curUser)) {
+    // 	const newUser = await this.userService.addNewUser(userData);
+    // 	return newUser;
+    // }
+    // TODO: refreshToken 이 이미 있는경우 처리
+    // return curUser;
+  }
 
+  async isTwoFactorCodeValid(twoFactorCode: string, user: User) {
+    if (!user.twoFactorSecret) {
+      return false;
+    }
+    return authenticator.verify({
+      token: twoFactorCode,
+      secret: user.twoFactorSecret,
+    });
+  }
 
+  async loginWith2fa(
+    userWithoutPw: Omit<User, "password">,
+  ): Promise<TokenSetDto> {
+    const tokenSet = new TokenSetDto();
+    tokenSet.refreshToken = await this.genRefreshToken(userWithoutPw, true);
+    tokenSet.accessToken = await this.genAccessToken(userWithoutPw, true);
+    return tokenSet;
+  }
 
-	async isTwoFactorCodeValid(twoFactorCode: string, user: User) {
-		if (!user.twoFactorSecret) {
-			return false;
-		}
-		return authenticator.verify({ token: twoFactorCode, secret: user.twoFactorSecret });
-	}
+  async login(userWithoutPw: Omit<User, "password">) {
+    const access_token = await this.genAccessToken(userWithoutPw, false);
+    const refresh_token = await this.genRefreshToken(userWithoutPw, false);
+    return { access_token, refresh_token };
+  }
 
-	async loginWith2fa(userWithoutPw: Omit<User, 'password'>) : Promise<TokenSetDto> {
-		const tokenSet = new TokenSetDto();
-		tokenSet.refreshToken = await this.genRefreshToken(userWithoutPw, true);
-		tokenSet.accessToken  = await this.genAccessToken(userWithoutPw, true);
-		return tokenSet;
-	}
+  async genAccessToken(user: Omit<User, "password">, twoFactor: boolean) {
+    const payload = {
+      uid: user.uid,
+      twoFactorEnabled: user.twoFactorEnabled,
+      twoFactorAuthenticated: twoFactor,
+    };
+    const access_token = this.jwtService.sign(payload);
+    return access_token;
+  }
 
-	async login(userWithoutPw: Omit<User, 'password'>) {
-		const access_token = await this.genAccessToken(userWithoutPw, false);
-		const refresh_token = await this.genRefreshToken(userWithoutPw, false);
-		return { access_token, refresh_token };
-	}
+  async genRefreshToken(user: Omit<User, "password">, twoFactor: boolean) {
+    const payload = {
+      refresh: true,
+      uid: user.uid,
+      twoFactorEnabled: user.twoFactorEnabled,
+      twoFactorAuthenticated: twoFactor,
+    };
+    const refreshToken = await this.jwtService.sign(payload, {
+      expiresIn: config.get<string>("jwt-refresh.exp"),
+    });
+    return refreshToken;
+  }
 
+  async refreshAccessToken(refresh_token: string, user: User) {
+    const refreshToken = refresh_token.split(" ")[1];
+    const refreshTokenPayload = refreshToken.split(".")[1];
+    const isMatch = await bcrypt.compare(
+      refreshTokenPayload,
+      user.refreshToken,
+    );
+    if (isMatch) {
+      return await this.genAccessToken(user, user.twoFactorEnabled);
+    } else {
+      const errMsg =
+        "The refresh token provided is invalid. Please log in again.";
+      Logger.error(errMsg);
+      throw new BadRequestException(errMsg);
+    }
+  }
 
-
-	async genAccessToken(user: Omit<User, 'password'>, twoFactor: boolean) {
-		const payload = {
-			uid: user.uid,
-			twoFactorEnabled: user.twoFactorEnabled,
-			twoFactorAuthenticated: twoFactor,
-		}
-		const access_token = this.jwtService.sign(payload);
-		return access_token;
-	}
-
-	async genRefreshToken(user: Omit<User, 'password'>, twoFactor: boolean) {
-		const payload = {
-			refresh: true,
-			uid: user.uid,
-			twoFactorEnabled: user.twoFactorEnabled,
-			twoFactorAuthenticated: twoFactor,
-		}
-		const refreshToken = await this.jwtService.sign(payload, { expiresIn: config.get<string>('jwt-refresh.exp')});
-		return refreshToken;	
-	}
-
-	// async verifyJwtToken(refreshToken: string): Promise<TokenPayload> {
-	// 	try {
-	// 		const payload = await this.jwtService.verify(refreshToken);
-	// 		return payload;
-	// 	}
-	// 	catch (error) {
-	// 		const errMsg = `Failed to verify the refresh token:  ${String(error)}`;
-	// 		Logger.error(errMsg);
-	// 		throw new BadRequestException(errMsg);
-	// 	}
-	// }
-
-	async refreshAccessToken(refresh_token: string, user: User) {
-		const refreshToken = refresh_token.split(' ')[1];
-		const refreshTokenPayload = refreshToken.split('.')[1];
-		const isMatch = await bcrypt.compare(refreshTokenPayload, user.refreshToken);
-		if (isMatch) {
-			return await this.genAccessToken(user, user.twoFactorEnabled);
-		}
-		else {
-			const errMsg = 'The refresh token provided is invalid. Please log in again.';
-			Logger.error(errMsg);
-			throw new UnauthorizedException(errMsg);
-		}
-	}
-
-	async jwtVerify(token: string): Promise<number> {
-		try {
-			const payload = await this.jwtService.verify(token);
-			return payload.uid;
-		} catch(e) {
-			console.log(e);
-			throw new UnauthorizedException("user not found");
-		}
-	}
-
+  async jwtVerify(token: string): Promise<number> {
+    try {
+      const payload = await this.jwtService.verify(token);
+      return payload.uid;
+    } catch (e) {
+      console.log(e);
+      throw new UnauthorizedException("user not found");
+    }
+  }
 }
--- backend/back/src/login/login.controller.ts
+++ backend/back/src/login/login.controller.ts
@@ -1,188 +1,249 @@
-import { Body, Controller, Get, Headers, Logger, Post, Query, Redirect, Req, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
-import * as swagger from '@nestjs/swagger';
-import config from 'config';
-import { Response } from 'express';
-import { AuthService } from 'src/auth/auth.service';
-import { Jwt2faAuthGuard } from 'src/auth/jwt-2fa/jwt-2fa-auth.guard';
-import { JwtRefreshGuard } from 'src/auth/jwt-refresh/jwt-refresh-auth.guard';
-import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
-import { LocalAuthGuard } from 'src/auth/local/local-auth.guard';
-import { callFunctionDescriptionOfRefreshRoute, ResponseErrorDto } from 'src/swagger/response.util';
-import { UserService } from 'src/user/user.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Logger,
+  Post,
+  Query,
+  Redirect,
+  Req,
+  Res,
+  UnauthorizedException,
+  UseGuards,
+} from "@nestjs/common";
+import * as swagger from "@nestjs/swagger";
+import config from "config";
+import { Response } from "express";
+import { AuthService } from "src/auth/auth.service";
+import { Jwt2faAuthGuard } from "src/auth/jwt-2fa/jwt-2fa-auth.guard";
+import { JwtRefreshGuard } from "src/auth/jwt-refresh/jwt-refresh-auth.guard";
+import { JwtAuthGuard } from "src/auth/jwt/jwt-auth.guard";
+import { LocalAuthGuard } from "src/auth/local/local-auth.guard";
+import {
+  callFunctionDescriptionOfRefreshRoute,
+  ResponseErrorDto,
+} from "src/swagger/response.util";
+import { UserService } from "src/user/user.service";
 
-@Controller('login')
-@swagger.ApiTags('로그인')
+@Controller("login")
+@swagger.ApiTags("로그인")
 export class LoginController {
-	constructor(
-		private authService: AuthService,
-		private userService: UserService,
-	) {
-	}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
 
-	@swagger.ApiOperation({
-		summary: '42 OAuth를 이용한 로그인 시도',
-		description: "Client를 42 OAuth로 리디렉트하여 인증 시도. 로그인에 성공하면 'code' query를 가지고 /login/oauth/callback으로 리디렉트.",
-	})
-	@swagger.ApiResponse({
-		status: 302,
-		description: '42 OAuth로 리디렉션 후, 로그인 시 /login/oauth/callback으로 리디렉션',
-	})
-	@Get('/oauth')
-	@Redirect(
-		'https://api.intra.42.fr/oauth/authorize?client_id=' + config.get<string>('intra.client_id') +
-		'&redirect_uri=' + config.get<string>('intra.redirect_uri') +
-		'&response_type=code', 302
-	)
-	intra() { }
+  @swagger.ApiOperation({
+    summary: "42 OAuth를 이용한 로그인 시도",
+    description:
+      "Client를 42 OAuth로 리디렉트하여 인증 시도. 로그인에 성공하면 'code' query를 가지고 /login/oauth/callback으로 리디렉트.",
+  })
+  @swagger.ApiResponse({
+    status: 302,
+    description:
+      "42 OAuth로 리디렉션 후, 로그인 시 /login/oauth/callback으로 리디렉션",
+  })
+  @Get("/oauth")
+  @Redirect(
+    "https://api.intra.42.fr/oauth/authorize?client_id=" +
+      config.get<string>("intra.client_id") +
+      "&redirect_uri=" +
+      config.get<string>("intra.redirect_uri") +
+      "&response_type=code",
+    302,
+  )
+  intra() {}
 
-	// frontend need to redirect user to 2fa auth page.
+  // frontend need to redirect user to 2fa auth page.
 
-	@swagger.ApiQuery({
-		name: 'code',
-		type: 'string',
-		description: '42 OAuth 로그인 콜백에서 받은 code',
-		required: true,
-		allowEmptyValue: false,
-	})
-	@swagger.ApiOperation({
-		summary: '42 OAuth 로그인 콜백',
-		description: '42 OAuth 로그인 콜백. 로그인에 성공하면 accessToken을 쿠키에 담고, refreshToken과 2fa 식별자(리디렉트 용)를 리턴.',
-	})
-	@swagger.ApiOkResponse({
-		description: 'ok',
-		schema: {
-			type: 'object',
-			properties: {
-				refreshToken: { type: 'string', description: '리프레쉬용 토큰 - refreshToken', },
-				redirect: { type: 'boolean', description: '2fa 인증이 필요한 경우 true, 아닌 경우 false', },
-			},
-			example: { refreshToken: '1NiIsInR5c6IkpXVCJ9.eyJ1axjoxNjgxNDcyOTA3fQ.24Dlhpwbv75GXMireozDpzVA', redirect: false, },
-		},
-	})
-	@swagger.ApiUnauthorizedResponse({ description: "bad 'code'", type: ResponseErrorDto })
-	@swagger.ApiInternalServerErrorResponse({ description: 'Intra server error try later or Using Id, Password', type: ResponseErrorDto })
-	@Get('/oauth/callback')
-	async intraSignIn(@Res() res: Response, @Query('code') code: string) {
-		const userData = await this.authService.intraSignIn(code);
-		const user = await this.userService.getUserByIntraDto(userData);
+  @swagger.ApiQuery({
+    name: "code",
+    type: "string",
+    description: "42 OAuth 로그인 콜백에서 받은 code",
+    required: true,
+    allowEmptyValue: false,
+  })
+  @swagger.ApiOperation({
+    summary: "42 OAuth 로그인 콜백",
+    description:
+      "42 OAuth 로그인 콜백. 로그인에 성공하면 accessToken을 쿠키에 담고, refreshToken과 2fa 식별자(리디렉트 용)를 리턴.",
+  })
+  @swagger.ApiOkResponse({
+    description: "ok",
+    schema: {
+      type: "object",
+      properties: {
+        refreshToken: {
+          type: "string",
+          description: "리프레쉬용 토큰 - refreshToken",
+        },
+        redirect: {
+          type: "boolean",
+          description: "2fa 인증이 필요한 경우 true, 아닌 경우 false",
+        },
+      },
+      example: {
+        refreshToken:
+          "1NiIsInR5c6IkpXVCJ9.eyJ1axjoxNjgxNDcyOTA3fQ.24Dlhpwbv75GXMireozDpzVA",
+        redirect: false,
+      },
+    },
+  })
+  @swagger.ApiUnauthorizedResponse({
+    description: "bad 'code'",
+    type: ResponseErrorDto,
+  })
+  @swagger.ApiInternalServerErrorResponse({
+    description: "Intra server error try later or Using Id, Password",
+    type: ResponseErrorDto,
+  })
+  @Get("/oauth/callback")
+  async intraSignIn(@Res() res: Response, @Query("code") code: string) {
+    const userData = await this.authService.intraSignIn(code);
+    const user = await this.userService.getUserByIntraDto(userData);
 
-		const tokenSet = await this.authService.login(user);
-		await this.userService.setUserRefreshToken(user, tokenSet.refresh_token);
+    const tokenSet = await this.authService.login(user);
+    await this.userService.setUserRefreshToken(user, tokenSet.refresh_token);
 
-		res.cookie('accessToken', tokenSet.access_token,
-			{
-				httpOnly: true,
-				sameSite: 'strict',
-				// secure: true //only https option
-			});
-		res.cookie('refreshToken', tokenSet.refresh_token);
-		return res.redirect('http://localhost:3000/');
-	}
+    res.cookie("accessToken", tokenSet.access_token, {
+      httpOnly: true,
+      sameSite: "strict",
+      // secure: true //only https option
+    });
+    res.cookie("refreshToken", tokenSet.refresh_token);
+    return res.redirect("http://localhost:3000/");
+  }
 
+  @swagger.ApiQuery({
+    description: "로그아웃 ",
+    required: true,
+  })
+  @swagger.ApiOperation({
+    summary: "로그아웃 요청 처리",
+    description: "AccessToken 제거, Refresh 토큰 제거(만료)",
+  })
+  @Post("/signout")
+  @UseGuards(Jwt2faAuthGuard)
+  async logout(@Req() request) {
+    const options = {
+      httpOnly: true,
+      sameSite: "strict",
+      // secure: true // only https option
+    };
+    request.cookie("access_token", "", { ...options, expires: new Date(0) });
+    return await this.userService.logout(request.user);
+  }
 
+  @swagger.ApiBearerAuth("refreshToken")
+  @swagger.ApiHeader({
+    name: "authorization",
+    description: callFunctionDescriptionOfRefreshRoute(
+      "Because too long to write in here",
+    ),
+    schema: {
+      default:
+        "비어있으면 테스트가 안되고, vaild한 token을 넣어도 테스트가 안되기에, 그냥 넣어놓은 테스트 문장",
+    },
+  })
+  @swagger.ApiOperation({
+    summary: "Refresh Token을 이용한 Access Token 재발급",
+    description:
+      "Refresh Token을 이용한 Access Token 재발급. Refresh Token이 유효하지 않거나 만료되면 400, 유효하지만 해당 유저가 없으면 401을 리턴.",
+  })
+  @swagger.ApiCreatedResponse({
+    description: "ok",
+    schema: {
+      type: "string",
+      properties: {
+        accessToken: { type: "string", description: "재발급된 accessToken" },
+      },
+      example: {
+        accessToken:
+          "1NiIsInR5c6IkpXVCJ9.eyJ1axjoxNjgxNDcyOTA3fQ.24Dlhpwbv75GXMireozDpzVA",
+      },
+    },
+  })
+  @swagger.ApiBadRequestResponse({
+    description: "Refresh Token이 유효하지 않거나 만료되었을 때",
+    type: ResponseErrorDto,
+  })
+  @swagger.ApiUnauthorizedResponse({
+    description: "Refresh Token이 유효하지만 해당 유저가 없을 때",
+    type: ResponseErrorDto,
+  })
+  @Post("/oauth/refresh")
+  @UseGuards(JwtRefreshGuard)
+  async refreshAccessTokens(
+    @Headers("authorization") refresh_token: string,
+    @Req() request,
+    @Res() res,
+  ) {
+    const user = request.user;
+    const regeneratedAccessToken = await this.authService.refreshAccessToken(
+      refresh_token,
+      user,
+    );
 
-	@swagger.ApiQuery({
-		description: '로그아웃 ',
-		required: true,
-	})
-	@swagger.ApiOperation({
-		summary: '로그아웃 요청 처리',
-		description: 'AccessToken 제거, Refresh 토큰 제거(만료)',
-	})
-	@Post('/signout')
-	@UseGuards(Jwt2faAuthGuard)
-	async logout(@Req() request) {
-		const options = {
-			httpOnly: true,
-			sameSite: 'strict',
-			// secure: true // only https option
-		  };
-		request.cookie('access_token', '', { ...options, expires: new Date(0) });
-		return await this.userService.logout(request.user);
-	}
+    res.cookie("accessToken", regeneratedAccessToken, {
+      httpOnly: true,
+      sameSite: "strict",
+      // secure: true //only https option
+    });
+    return res.redirect("http://localhost:3000/");
+  }
 
+  // /2fa/auth will return new accessToken.
+  @Post("/2fa/auth")
+  @UseGuards(JwtAuthGuard)
+  async auth(@Req() request, @Body() body) {
+    Logger.log("trying 2fa login");
+    const isCodeValid = await this.authService.isTwoFactorCodeValid(
+      body.twoFactorCode,
+      request.user,
+    );
+    if (!isCodeValid) {
+      throw new UnauthorizedException("Wrong authentication code");
+    }
+    const tokens = await this.authService.loginWith2fa(request.user);
+    request.cookie("accessToken", tokens.accessToken, {
+      httpOnly: true,
+      sameSite: "strict",
+      // secure: true //only https option
+    });
+    await this.userService.setUserRefreshToken(
+      request.user,
+      tokens.refreshToken,
+    );
+  }
 
-	@swagger.ApiBearerAuth('refreshToken')
-	@swagger.ApiHeader({
-		name: 'authorization',
-		description: callFunctionDescriptionOfRefreshRoute('Because too long to write in here'),
-		schema: { default: '비어있으면 테스트가 안되고, vaild한 token을 넣어도 테스트가 안되기에, 그냥 넣어놓은 테스트 문장', }
-	})
-	@swagger.ApiOperation({
-		summary: 'Refresh Token을 이용한 Access Token 재발급',
-		description: 'Refresh Token을 이용한 Access Token 재발급. Refresh Token이 유효하지 않거나 만료되면 400, 유효하지만 해당 유저가 없으면 401을 리턴.',
-	})
-	@swagger.ApiCreatedResponse({
-		description: 'ok',
-		schema: {
-			type: 'string',
-			properties: { accessToken: { type: 'string', description: '재발급된 accessToken', }, },
-			example: { accessToken: '1NiIsInR5c6IkpXVCJ9.eyJ1axjoxNjgxNDcyOTA3fQ.24Dlhpwbv75GXMireozDpzVA', },
-		},
-	})
-	@swagger.ApiBadRequestResponse({ description: 'Refresh Token이 유효하지 않거나 만료되었을 때', type: ResponseErrorDto })
-	@swagger.ApiUnauthorizedResponse({ description: 'Refresh Token이 유효하지만 해당 유저가 없을 때', type: ResponseErrorDto })
-	@Post('/oauth/refresh')
-	@UseGuards(JwtRefreshGuard)
-	async refreshAccessTokens(@Headers('authorization') refresh_token: string, @Req() request) {
-		const user = request.user;
-		return await this.authService.refreshAccessToken(refresh_token, user);
-	}
+  // login with email & password.
+  @Post("/email")
+  @UseGuards(LocalAuthGuard)
+  loginWithEmail(@Req() request) {
+    if (request.user.twoFactorEnabled) {
+      throw new UnauthorizedException("2fa required.");
+    }
+    return this.authService.genAccessToken(request.user, false);
+  }
+  //For Debug Controller
+  @Post("/test")
+  @UseGuards(Jwt2faAuthGuard)
+  testFunc(): string {
+    return "success";
+  }
 
-	
-	// /2fa/auth will return new accessToken.
-	@Post('/2fa/auth')
-	@UseGuards(JwtAuthGuard)
-	async auth(@Req() request, @Body() body) {
-		Logger.log('trying 2fa login');
-		const isCodeValid = await this.authService.isTwoFactorCodeValid(
-			body.twoFactorCode,
-			request.user,
-		);
-		if (!isCodeValid) {
-			throw new UnauthorizedException('Wrong authentication code');
-		}
-		const tokens = await this.authService.loginWith2fa(request.user);
-		request.cookie('accessToken', tokens.accessToken,
-		{
-			httpOnly: true,
-			sameSite: 'strict',
-			// secure: true //only https option
-		}
-		);
-		await this.userService.setUserRefreshToken(request.user, tokens.refreshToken);
-	}
-	
-	// login with email & password.
-	@Post('/email')
-	@UseGuards(LocalAuthGuard)
-	loginWithEmail(@Req() request) {
-		if (request.user.twoFactorEnabled) {
-			throw new UnauthorizedException('2fa required.');
-		}
-		return this.authService.genAccessToken(request.user, false);
-	}
-	//For Debug Controller
-	@Post('/test')
-	@UseGuards(Jwt2faAuthGuard)
-	testFunc(): string {
-		return 'success';
-	}
+  @UseGuards(Jwt2faAuthGuard)
+  @Get("/user")
+  showUsers(@Req() request) {
+    console.log(request);
+    return this.userService.showUsers();
+  }
 
-	@UseGuards(Jwt2faAuthGuard)
-	@Get('/user')
-	showUsers(@Req() request) {
-		console.log(request);
-		return this.userService.showUsers();
-	}
-
-	// For Test
-	@Get('/logout')
-	@UseGuards(Jwt2faAuthGuard)
-	async red(@Req() request){
-		await this.logout(request);
-	}
-	
-
+  // For Test
+  @Get("/logout")
+  @UseGuards(Jwt2faAuthGuard)
+  async red(@Req() request) {
+    await this.logout(request);
+  }
 }
-	
