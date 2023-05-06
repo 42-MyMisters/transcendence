f8b5d88b4485dd38f16edce83325a0d2d537e059
Yoma <codeyoma@gmail.com>
2023-05-07 04:29:44 +0900

    profile page friends color

--- frontend/front/src/components/ProfilePage/ProfileFriend.tsx
+++ frontend/front/src/components/ProfilePage/ProfileFriend.tsx
@@ -24,7 +24,7 @@ export default function ProfileFriend() {
                   chat={'normal'}
                   power="member"
                   callBack={() => { }}
-                  defaultColor={"#eee"}
+                  defaultColor={"#111"}
                 />
               );
             })
