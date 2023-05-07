8fc55b2bb484556872b583578a0e0be31dbff4ed
YunKi <yunki.dev@gmail.com>
2023-05-07 22:21:44 +0900

    [fix] details

--- frontend/front/src/components/GamePage/GameInviteModal.tsx
+++ frontend/front/src/components/GamePage/GameInviteModal.tsx
@@ -7,7 +7,7 @@ export default function GameInviteModal({
   AcceptBtn,
 }: {
   from: string;
-  AcceptBtn: () => {};
+  AcceptBtn: () => void;
 }) {
   const [, setGameInviteModal] = useAtom(gameInviteModalAtom);
 
