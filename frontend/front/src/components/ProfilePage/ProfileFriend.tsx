c1074d7691f81cc0c05ac7a5abfd1082055998d8
Yoma <codeyoma@gmail.com>
2023-05-08 16:26:54 +0900

    edit profile_page : bring data from following data to  userList

--- frontend/front/src/components/ProfilePage/ProfileFriend.tsx
+++ frontend/front/src/components/ProfilePage/ProfileFriend.tsx
@@ -3,16 +3,19 @@ import UserObj from "../objects/UserObj";
 
 import { useAtom } from "jotai";
 import { UserAtom } from "../atom/UserAtom";
+import * as chatAtom from "../atom/ChatAtom";
 
 export default function ProfileFriend() {
   const [userInfo] = useAtom(UserAtom);
+  const [userList, setUserList] = useAtom(chatAtom.userListAtom);
+  const [followingList, setFollowingList] = useAtom(chatAtom.followingListAtom);
 
   return (
     <div className="ProfileFriendFrame">
       <div className="ProfileFriendTitle">friends</div>
       <div className="ProfileFriendBG">
         <div className="ProfileFriendList">
-          {userInfo.followings
+          {/* {userInfo.followings
             ? userInfo.followings.map((key) => {
               return (
                 <UserObj
@@ -28,7 +31,24 @@ export default function ProfileFriend() {
                 />
               );
             })
-            : null}
+            : null} */}
+          {
+            Object.keys(followingList).map((key) => {
+              return (
+                <UserObj
+                  key={key}
+                  uid={Number(key)}
+                  nickName={userList[Number(key)].userDisplayName}
+                  profileImage={userList[Number(key)].userProfileUrl}
+                  status={userList[Number(key)].userStatus}
+                  chat={'normal'}
+                  power="member"
+                  callBack={() => { }}
+                  defaultColor={"#111"}
+                />
+              );
+            })
+          }
         </div>
       </div>
     </div>
