31be104ac262468ed48d3746baefc34224205590
yoma in 42 cluster <yongmkim@student.42seoul.kr>
2023-05-17 20:45:50 +0900

    create game socket in gamePage component

--- frontend/front/src/socket/game.socket.ts
+++ frontend/front/src/socket/game.socket.ts
@@ -8,33 +8,33 @@ const URL = "https://localhost";
 
 const GameNameSpace = "/game";
 
-export const gameSocket = io(`${URL}${GameNameSpace}`, {
-  auth: (cb) => {
-    cb({
-      token: localStorage.getItem("refreshToken")
-    });
-  },
-  autoConnect: false,
-  transports: ["polling", "websocket"],
-  secure: true,
-  upgrade: true,
-  // reconnectionDelay: 1000, // defaults to 1000
-  // reconnectionDelayMax: 10000, // defaults to 5000
-  // withCredentials: true,
-  // path: "/socket.io",
-});
+// export const gameSocket = io(`${URL}${GameNameSpace}`, {
+//   auth: (cb) => {
+//     cb({
+//       token: localStorage.getItem("refreshToken")
+//     });
+//   },
+//   autoConnect: false,
+//   transports: ["polling", "websocket"],
+//   secure: true,
+//   upgrade: true,
+//   // reconnectionDelay: 1000, // defaults to 1000
+//   // reconnectionDelayMax: 10000, // defaults to 5000
+//   // withCredentials: true,
+//   // path: "/socket.io",
+// });
 
-export function emitUpPress() {
-  gameSocket.emit("upPress");
-}
-export function emitUpRelease() {
-  gameSocket.emit("upRelease");
-}
+// export function emitUpPress() {
+//   gameSocket.emit("upPress");
+// }
+// export function emitUpRelease() {
+//   gameSocket.emit("upRelease");
+// }
 
-export function emitDownPress() {
-  gameSocket.emit("downPress");
-}
+// export function emitDownPress() {
+//   gameSocket.emit("downPress");
+// }
 
-export function emitDownRelease() {
-  gameSocket.emit("downRelease");
-}
+// export function emitDownRelease() {
+//   gameSocket.emit("downRelease");
+// }
