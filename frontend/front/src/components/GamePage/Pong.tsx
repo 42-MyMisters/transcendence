2bf16277006e9a6905136b7017f5cc9fb64ff045
yoma in 42 cluster <yongmkim@student.42seoul.kr>
2023-05-24 16:14:07 +0900

    add typeof check in chat.gateway

--- frontend/front/src/components/GamePage/Pong.tsx
+++ frontend/front/src/components/GamePage/Pong.tsx
@@ -104,7 +104,7 @@ export function Game(gameInfo: GameCoordinate, canvas: React.RefObject<HTMLCanva
     drawText(player1.score, WIDTH / 4, 50, "WHITE", context);
     drawText(player2.score, 3 * (WIDTH / 4), 50, "WHITE", context);
 
-    drawCounter(3, (WIDTH / 2) - 50, (HEIGHT / 2) + 75, "WHITE", context);
+    drawCounter(1, (WIDTH / 2) - 66, (HEIGHT / 2) + 75, "WHITE", context);
 
     drawName(player1.name, 75, 50, "WHITE", context);
     drawName(player2.name, (WIDTH / 4) * 3 + 50, 50, "WHITE", context);
