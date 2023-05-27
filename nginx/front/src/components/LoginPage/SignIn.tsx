98a7101f5082404b04f723d9ca86a33fd69c4a4f
space <enaenen@naver.com>
2023-05-28 02:02:05 +0900

    css change

--- nginx/front/src/components/LoginPage/SignIn.tsx
+++ nginx/front/src/components/LoginPage/SignIn.tsx
@@ -4,13 +4,21 @@ export default function SignInModal() {
   function handleLoginBtn() {
     window.location.href = `${String(process.env.REACT_APP_API_URL)}/login/oauth`;
   }
-
+  function handleIDPWLoginBtn(){
+    window.location.href = `${String(process.env.REACT_APP_API_URL)}/login/`
+  }
+    function handleRegisterBtn(){
+    window.location.href = `${String(process.env.REACT_APP_API_URL)}/register/`
+  }
   return (
     <div className="LoginModalsBG">
-      <div className="LoginTxt">Sign In</div>
-      <button className="LoginBtn" onClick={handleLoginBtn}>
-        Intra
-      </button>
+      <div className="login-form">
+          <input type="text" name="email" className="text-field" placeholder="email" />
+          <input type="password" name="password" className="text-field" placeholder="password" />
+          <button className="submit-btn" onClick={handleIDPWLoginBtn}> Login </button> 
+          <button className="submit-btn" onClick={handleLoginBtn} > 42 Intra Login </button>
+          <button className="register-btn" onClick={handleRegisterBtn} > Register </button>
+      </div>
     </div>
   );
 }
